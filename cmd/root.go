39077f171071866c27bacbda50ab8495e0ad701b
Joko Irianto <irianto94@outlook.com>
2019-08-24 18:49:05 +0700

    add gitignore

--- cmd/root.go
+++ cmd/root.go
@@ -32,13 +32,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
   Use:   "vending-machine-cli",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  Short: "Vending Machinge command line interface",
+  Long: `This is a console program of Vending Machinge command line interface`,
   // Uncomment the following line if your bare application
   // has an action associated with it:
   //	Run: func(cmd *cobra.Command, args []string) { },
